cfbe0d778d9c29598b04e8c20c1875340b140fca
Dushyant Mathur <dushyantmathur08@gmail.com>
2025-08-02 18:04:38 +0530

    added changes for be deployment

--- backend/index.js
+++ backend/index.js
@@ -12,8 +12,10 @@ app.use(cors());
 
 app.use('/api/v1',mainRouter);
 
-app.listen(3000, () => {
-    console.log('Server running on port 3000');
-});  
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log('Server running on port', PORT);
+});
+
 
  
